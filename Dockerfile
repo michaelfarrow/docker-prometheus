6f1c09e742118e0a7d302adcf0cd666869653ff1
Mike Farrow <contact@mikefarrow.co.uk>
2015-12-30 22:25:52 +0000

    added relabel config

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,12 @@ FROM prom/prometheus:0.16.1
 MAINTAINER Mike Farrow <contact@mikefarrow.co.uk>
 
 ENV DNS_SD_NAMES ''
+ENV JOB_NAME prometheus
 ENV SCRAPE_INTERVAL 30s
+
+ENV RELABEL_ACTION replace
+ENV RELABEL_REGEX (.*)
+ENV RELABEL_REPLACEMENT '${1}'
 
 ADD config/prometheus.yml /etc/prometheus/prometheus.yml
 
--- config/start.sh
+++ config/start.sh
@@ -8,6 +8,16 @@ done
 
 sed -i "s/%dns_sd_names%/$FORMATTED_DNS_SD_NAMES/" /etc/prometheus/prometheus.yml
 sed -i "s/%scrape_interval%/$SCRAPE_INTERVAL/" /etc/prometheus/prometheus.yml
+sed -i "s/%job_name%/$JOB_NAME/" /etc/prometheus/prometheus.yml
+sed -i "s/%auth_user%/$AUTH_USER/" /etc/prometheus/prometheus.yml
+sed -i "s/%auth_pass%/$AUTH_PASSWORD/" /etc/prometheus/prometheus.yml
+
+sed -i "s/%relabel_source_labels%/$RELABEL_SOURCE_LABELS/" /etc/prometheus/prometheus.yml
+sed -i "s/%relabel_regex%/$RELABEL_REGEX/" /etc/prometheus/prometheus.yml
+sed -i "s/%relabel_replacement%/$RELABEL_REPLACEMENT/" /etc/prometheus/prometheus.yml
+sed -i "s/%relabel_modulus%/$RELABEL_MODULUS/" /etc/prometheus/prometheus.yml
+sed -i "s/%relabel_target_label%/$RELABEL_TARGET_LABEL/" /etc/prometheus/prometheus.yml
+sed -i "s/%relabel_action%/$RELABEL_ACTION/" /etc/prometheus/prometheus.yml
 
 cat /etc/prometheus/prometheus.yml
 
